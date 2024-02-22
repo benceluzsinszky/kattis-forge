d26db95bb03172aac4318292706291a5c156226b
bence <benceluzsinszky@gmail.com>
2024-02-22 22:05:44 +0100

    added more feedback to answercheckers

--- scripts/answerchecker_java.bat
+++ scripts/answerchecker_java.bat
@@ -1,17 +1,19 @@
 @echo off
 setlocal enabledelayedexpansion
 javac Main.java
+set /a counter=1
 for %%i in (samples\*.in) do (
+    echo Test !counter!
     java Main < %%i > temp.out
     fc /n temp.out samples\%%~ni.ans > nul
     set /p output=<temp.out
     set /p ans=<samples\%%~ni.ans
     if errorlevel 1 (
-        echo Fail !output! !ans!
+        echo Fail ^| Expected: !ans! ^| Output: !output!
     ) else (
-        echo Success !output! !ans!
+        echo Success ^| Expected: !ans! ^| Output: !output!
     )
+    set /a counter+=1
 )
-
 del temp.out
 pause
--- scripts/answerchecker_python.bat
+++ scripts/answerchecker_python.bat
@@ -1,16 +1,18 @@
 @echo off
 setlocal enabledelayedexpansion
+set /a counter=1
 for %%i in (samples\*.in) do (
+    echo Test !counter!
     python main.py < %%i > temp.out
     fc /n temp.out samples\%%~ni.ans > nul
     set /p output=<temp.out
     set /p ans=<samples\%%~ni.ans
     if errorlevel 1 (
-        echo Fail !output! !ans!
+        echo Fail ^| Expected: !ans! ^| Output: !output!
     ) else (
-        echo Success !output! !ans!
+        echo Success ^| Expected: !ans! ^| Output: !output!
     )
+    set /a counter+=1
 )
-
 del temp.out
 pause
